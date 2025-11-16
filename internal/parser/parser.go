d31d013e95457a07451f6c4633c377362e9b59c8
Adit0507 <adi05072002@gmail.com>
2025-11-16 10:26:25 +0530

    methods for error handling & parsing logic

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,5 +1,7 @@
 package parser
 
+import "fmt"
+
 type Parser struct {
 	lexer     *Lexer
 	curToken  Token
@@ -10,9 +12,9 @@ type Parser struct {
 func NewParser(input string) *Parser {
 	l := NewLexer(input)
 	p := &Parser{
-		lexer: l,
+		lexer:  l,
 		errors: []string{},
-	} 
+	}
 
 	// readin 2 tokens to initialize currtoken and peektoken
 	p.nextToken()
@@ -21,7 +23,7 @@ func NewParser(input string) *Parser {
 	return p
 }
 
-func (p *Parser) nextToken () {
+func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.lexer.NextToken()
 }
@@ -30,4 +32,23 @@ func (p *Parser) Errors() []string {
 	return p.errors
 }
 
+func (p *Parser) addError(msg string) {
+	p.errors = append(p.errors, fmt.Sprintf("Line %d, Col %d: %s", p.curToken.Line, p.curToken.Column, msg))
+}
 
+func (p *Parser) curTokenIs(t TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	}
+	p.addError(fmt.Sprintf("expected %s, got %s", t, p.peekToken.Type))
+	return false
+}
