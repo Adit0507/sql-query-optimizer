c579aed09eb2eb4d9a9572b2435c983ed6586b91
Adit0507 <adi05072002@gmail.com>
2025-11-19 22:24:03 +0530

    lexer test

---
+++ internal/parser/parser_test.go
@@ -0,0 +1,39 @@
+package parser
+
+import (
+	"testing"
+)
+
+func TestLexer(t *testing.T) {
+	input := `SELECT id, name FROM users WHERE id = 5`
+
+	tests := []struct {
+		expectedType    TokenType
+		expectedLiteral string
+	}{
+		{SELECT, "SELECT"},
+		{IDENT, "id"},
+		{COMMA, ","},
+		{IDENT, "name"},
+		{FROM, "FROM"},
+		{IDENT, "users"},
+		{WHERE, "WHERE"},
+		{IDENT, "id"},
+		{EQ, "="},
+		{INT, "5"},
+		{EOF, ""},
+	}
+
+	l := NewLexer(input)
+	for i, ttt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != ttt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, ttt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != ttt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, ttt.expectedLiteral, tok.Literal)
+		}
+	}
+}
