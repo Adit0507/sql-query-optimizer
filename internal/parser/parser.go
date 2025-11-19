7eccbe1a11496f3790e8d76935966e43977f02cb
Adit0507 <adi05072002@gmail.com>
2025-11-19 22:06:46 +0530

    implemented full select statement parsing with expressions, joins and column handling

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,6 +1,9 @@
 package parser
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type Parser struct {
 	lexer     *Lexer
@@ -51,4 +54,240 @@ func (p *Parser) expectPeek(t TokenType) bool {
 	}
 	p.addError(fmt.Sprintf("expected %s, got %s", t, p.peekToken.Type))
 	return false
+}
+
+func (p *Parser) Parse() Statement {
+	if p.curTokenIs(SELECT) {
+		return p.parseSelectStatement()
+	}
+	p.addError(fmt.Sprintf("unexpcted token %s", p.curToken.Type))
+
+	return nil
+}
+
+func (p *Parser) parseSelectStatement() *SelectStatement {
+	stmt := &SelectStatement{}
+
+	if !p.expectPeek(ASTERISK) && !p.peekTokenIs(IDENT) {
+		return nil
+	}
+
+	// parsing select columsn
+	p.nextToken()
+	stmt.Columns = p.parseSelectColumns()
+
+	if !p.expectPeek(FROM) {
+		return nil
+	}
+
+	p.nextToken()
+	stmt.From = p.parseTableRef()
+
+	// parsing optional JOINs
+	for p.peekTokenIs(JOIN) || p.peekTokenIs(INNER) || p.peekTokenIs(LEFT) || p.peekTokenIs(RIGHT) {
+		p.nextToken()
+
+		join := p.parseJoinClause()
+		if join != nil {
+			stmt.Joins = append(stmt.Joins, join)
+		}
+	}
+
+	// parse optional WHERE clause
+	if p.peekTokenIs(WHERE) {
+		p.nextToken()
+		p.nextToken() //movin to next column
+
+		stmt.Where = p.parseExpression()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseExpression() Expression {
+	return p.parseOrExpression()
+}
+
+func (p *Parser) parseOrExpression() Expression {
+	left := p.parseAndExpression()
+
+	for p.peekTokenIs(OR) {
+		p.nextToken()
+
+		op := p.curToken.Literal
+		p.nextToken()
+		right := p.parseAndExpression()
+		left = &BinaryExpr{
+			Left:     left,
+			Operator: op,
+			Right:    right,
+		}
+	}
+
+	return left
+}
+
+func (p *Parser) parseAndExpression() Expression {
+	left := p.parseComparisionExpression()
+	
+	for p.peekTokenIs(AND) {
+		p.nextToken()
+		op := p.curToken.Literal
+		p.nextToken()
+		right := p.parseComparisionExpression()
+		left = &BinaryExpr{
+			Left:     left,
+			Operator: op,
+			Right:    right,
+		}
+	}
+	
+	return left
+}
+func (p *Parser) parseComparisionExpression() Expression {
+	left := p.parsePrimaryExpression()
+
+	if p.peekTokenIs(EQ) || p.peekTokenIs(NEQ) || p.peekTokenIs(LT) || p.peekTokenIs(LTE) || p.peekTokenIs(GT) || p.peekTokenIs(GTE) {
+		p.nextToken()
+		op := p.curToken.Literal
+		p.nextToken()
+		right := p.parsePrimaryExpression()
+
+		return &BinaryExpr{
+			Left:     left,
+			Operator: op,
+			Right:    right,
+		}
+	}
+
+	return left
+}
+
+func (p *Parser) parsePrimaryExpression() Expression {
+	switch p.curToken.Type {
+	case IDENT:
+		return p.parseColumnRef()
+
+	case INT:
+		val, _ := strconv.Atoi(p.curToken.Literal)
+		return &Literal{Type: IntLiteral, Value: val}
+
+	case STRING:
+		return &Literal{Type: StringLiteral, Value: p.curToken.Literal}
+	case LPAREN:
+		p.nextToken()
+		expr := p.parseExpression()
+		if !p.expectPeek(RPAREN) {
+			return nil
+		}
+
+		return expr
+
+	default:
+		p.addError(fmt.Sprintf("unexpected token in expression: %s", p.curToken.Type))
+		return nil
+	}
+}
+
+func (p *Parser) parseJoinClause() *JoinClause {
+	join := &JoinClause{}
+
+	//join type
+	if p.curTokenIs(INNER) {
+		join.Type = InnerJoin
+		if !p.expectPeek(JOIN) {
+			return nil
+		}
+	} else if p.curTokenIs(LEFT) {
+		join.Type = LeftJoin
+		if !p.expectPeek(JOIN) {
+			return nil
+		}
+	} else if p.curTokenIs(RIGHT) {
+		join.Type = RightJoin
+		if !p.expectPeek(JOIN) {
+			return nil
+		}
+	} else {
+		join.Type = InnerJoin
+	}
+
+	// parse table
+	if !p.expectPeek(IDENT) {
+		return nil
+	}
+	join.Table = p.parseTableRef()
+
+	// parse on condition
+	if !p.expectPeek(ON) {
+		return nil
+	}
+	p.nextToken()
+	join.Condition = p.parseExpression()
+
+	return join
+}
+
+func (p *Parser) parseTableRef() *TableRef {
+	table := &TableRef{
+		Name: p.curToken.Literal,
+	}
+
+	if p.peekTokenIs(AS) {
+		p.nextToken()
+		if !p.expectPeek(IDENT) {
+			return nil
+		}
+
+		table.Alias = p.curToken.Literal
+	} else if p.peekTokenIs(IDENT) {
+		p.nextToken()
+		table.Alias = p.curToken.Literal
+	}
+
+	return table
+}
+
+func (p *Parser) parseColumnRef() Expression {
+	col := &ColumnRef{
+		Column: p.curToken.Literal,
+	}
+
+	// checking for table.colum syntax
+	if p.peekTokenIs(DOT) {
+		col.Table = col.Column
+		p.nextToken()
+
+		if !p.expectPeek(IDENT) && !p.expectPeek(ASTERISK) {
+			return nil
+		}
+		if p.curTokenIs(ASTERISK) {
+			return &StarExpr{Table: col.Table}
+		}
+
+		col.Column = p.curToken.Literal
+	}
+
+	return col
+}
+
+func (p *Parser) parseSelectColumns() []Expression {
+	var cols []Expression
+
+	if p.curTokenIs(ASTERISK) {
+		cols = append(cols, &StarExpr{})
+		return cols
+	}
+
+	// parsin first column
+	cols = append(cols, p.parseColumnRef())
+
+	for p.peekTokenIs(COMMA) {
+		p.nextToken()
+		p.nextToken()
+
+		cols = append(cols, p.parseColumnRef())
+	}
+
+	return cols
 }
