1fe48a58121b0269b384344966adb41ca98b1e64
Adit0507 <adi05072002@gmail.com>
2025-11-23 21:15:29 +0530

    defining table metadata, and methods to load metadata from json file & adding the table to catalog

--- internal/catalog/catalog.go
+++ internal/catalog/catalog.go
@@ -1,5 +1,11 @@
 package catalog
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 type DataType int //simple col types
 
 const (
@@ -36,4 +42,46 @@ type Statistics struct { //table statistics for cost estimation
 	RowCount      int            `json:"row_count"`
 	DistinctCount map[string]int `json:"distinct_count"`
 	NullCount     map[string]int `json:"null_count"`
+}
+
+type TableInfo struct { //metadata about table
+	Name       string      `json:"name"`
+	Columns    []Column    `json:"columns"`
+	Indexes    []Index     `json:"indexes"`
+	Statistics *Statistics `json:"statistics"`
+	DataFile   string      `json:"data_file"` //pat to json datafile
+}
+
+type Catalog struct {
+	tables map[string]*TableInfo
+}
+
+func NewCatalog() *Catalog {
+	return &Catalog{
+		tables: make(map[string]*TableInfo),
+	}
+}
+
+// loading metdata from json fle
+func (c *Catalog) LoadFromFile(filepath string) error {
+	data, err := os.ReadFile(filepath)
+	if err != nil {
+		return fmt.Errorf("failed to read catalog file: %w", err)
+	}
+
+	var tables []TableInfo
+	if err := json.Unmarshal(data, &tables); err != nil {
+		return fmt.Errorf("failed to parse catalog %w", err)
+	}
+
+	for i := range tables {
+		c.tables[tables[i].Name] = &tables[i]
+	}
+
+	return nil
+}
+
+// adds a table to catalog
+func (c *Catalog) RegisterTable(table *TableInfo) {
+	c.tables[table.Name] = table
 }
