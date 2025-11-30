dd62c9c6094c95f655e477c3d61871321371ea8a
Adit0507 <adi05072002@gmail.com>
2025-11-30 23:06:02 +0530

    methods for SELECT column

--- internal/plan/logical.go
+++ internal/plan/logical.go
@@ -53,3 +53,27 @@ func (l *LogicalFilter) String() string {
 type Expr interface {
 	String() string
 }
+
+// SELECT column list
+type LogicalProject struct {
+	Input       LogicalPlan
+	Projections []Expr
+	ColumnNames []string
+}
+
+func (l *LogicalProject) Children() []LogicalPlan {
+	return []LogicalPlan{l.Input}
+}
+
+func (l *LogicalProject) Schema() []catalog.Column {
+	cols := make([]catalog.Column, len(l.ColumnNames))
+
+	for i, name := range l.ColumnNames { //columnnames based on projections
+		cols[i] = catalog.Column{Name: name, Type: catalog.StringType}
+	}
+
+	return cols
+}
+func (l *LogicalProject) String() string {
+	return fmt.Sprintf("Project(%v)", l.ColumnNames)
+}
