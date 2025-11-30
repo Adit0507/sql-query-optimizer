df5606553aa791aa0a8f74766eb42cdd2730b866
Adit0507 <adi05072002@gmail.com>
2025-11-30 22:41:09 +0530

    methods for table scan

--- internal/plan/logical.go
+++ internal/plan/logical.go
@@ -1,6 +1,10 @@
 package plan
 
-import "github.com/Adit0507/sql-query-optimizer/internal/catalog"
+import (
+	"fmt"
+
+	"github.com/Adit0507/sql-query-optimizer/internal/catalog"
+)
 
 type LogicalPlan interface {
 	Children() []LogicalPlan
@@ -8,4 +12,24 @@ type LogicalPlan interface {
 	String() string
 }
 
+// representin table scan
+type LogicalScan struct {
+	TableName string
+	Table     *catalog.TableInfo
+	Alias     string
+}
 
+func (l *LogicalScan) Children() []LogicalPlan {
+	return nil
+}
+func (l *LogicalScan) Schema() []catalog.Column {
+	return  l.Table.Columns
+}
+
+func (l *LogicalScan) String() string {
+	if l.Alias != ""{
+		return fmt.Sprintf("Scan(%s AS %s)", l.TableName, l.Alias)
+	}	
+
+	return fmt.Sprintf("Scan(%s)", l.TableName)
+}
